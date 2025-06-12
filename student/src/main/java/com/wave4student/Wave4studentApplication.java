b34e26d951c77b873e91bc18b3651dc53d14cbf3
VyshnaviNukala <84068358+VyshnaviNukala@users.noreply.github.com>
2025-06-12 21:21:45 +0530

    Add files via upload

---
+++ student/src/main/java/com/wave4student/Wave4studentApplication.java
@@ -0,0 +1,13 @@
+package com.wave4student;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Wave4studentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Wave4studentApplication.class, args);
+	}
+
+}
---
+++ student/src/test/java/com/wave4student/Wave4studentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wave4student;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Wave4studentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
